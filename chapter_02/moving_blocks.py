65708cbf4681583cb1c400768b008c4be3e7ccb3
Jen Leadbetter <j.leadbetter@dickblick.com>
2016-05-30 06:27:42 -0400

    [chapter 02] generating child nodes

--- chapter_02/moving_blocks.py
+++ chapter_02/moving_blocks.py
@@ -24,25 +24,22 @@ class BlockConfigurationNode(AbstractNode):
 
     _allowed_moves = {}
 
-    PILE1_REPR = 0
-    PILE2_REPR = 1
-
     PILE1_INDEX = 0
-    HAND_INDEX = 1
-    PILE2_INDEX = 2
-    HAND_POS_INDEX = 3
+    PILE2_INDEX = 1
+    HAND_POS_INDEX = 2
 
     RED_INDEX = 0
     BLUE_INDEX = 1
 
-    def __init__(self, state_for_red, state_for_blue,
-                 total_red, total_blue, total_hands,
-                 allowed_moves, parent=None):
+    def __init__(self, state_for_red, state_for_blue, hand_location,
+                 total_red, total_blue, total_hands, parent=None):
         """
         Creates a node for use in the 'Block Game' problem.
 
-        :param state_for_red: tuple in the form (pile1, pile2, hand_location)
-        :param state_for_blue: tuple in the form (pile1, pile2, hand_location)
+        :param state_for_red: tuple in the form (pile1, pile2)
+        :param state_for_blue: tuple in the form (pile1, pile2)
+        :param hand_location: integer, which pile the hands are currently by. Use
+                either PILE1_INDEX or PILE2_INDEX
         :param total_red: integer, expected total number of red blocks
         :param total_blue: integer, expected total number of blue blocks
         :param total_hands: integer, how many hands can move blocks
@@ -50,6 +47,7 @@ class BlockConfigurationNode(AbstractNode):
         """
         self._red = total_red
         self._blue = total_blue
+        self._hand_loc = hand_location
         self._hands = total_hands
         self._parent = parent
 
@@ -79,6 +77,16 @@ class BlockConfigurationNode(AbstractNode):
         pile2_count = self._state[game_state_index][self.PILE2_INDEX]
         return pile1_count, pile2_count
 
+    def _new_move(self, old_move_tuple, new_move_tuple):
+        move = []
+        move[self.PILE1_INDEX] = old_move_tuple[self.PILE1_INDEX] + new_move_tuple[self.PILE1_INDEX]
+        move[self.PILE2_INDEX] = old_move_tuple[self.PILE2_INDEX] + new_move_tuple[self.PILE2_INDEX]
+        return move
+
+    def _new_child_node(self, new_red_state, new_blue_state, new_hand_location):
+        return BlockConfigurationNode(new_red_state, new_blue_state, new_hand_location,
+                                       self._red, self._blue, self._hands, self)
+
     def allowed_moves(self):
         """
         Generates or gets from memoization valid moves for each pile of blocks.
@@ -101,16 +109,16 @@ class BlockConfigurationNode(AbstractNode):
         return self._allowed_moves
 
     @staticmethod
-    def state_for_block_type(pile1, hand, pile2, hand_location):
+    def state_for_block_type(pile1, pile2):
         # While it looks like the method is just returning *args, this is a means of
         # decoupling so we can change the representation later without a complete rewrite
-        return (pile1, hand, pile2, hand_location)
+        return pile1, pile2
 
     @staticmethod
-    def game_state(state_for_red, state_for_blue):
+    def game_state(state_for_red, state_for_blue, current_hand):
         # While it looks like the method is just returning *args, this is a means of
         # decoupling so we can change the representation later without a complete rewrite
-        return (state_for_red, state_for_blue)
+        return state_for_red, state_for_blue, current_hand
 
     def is_valid(self):
         return self._valid
@@ -120,8 +128,12 @@ class BlockConfigurationNode(AbstractNode):
 
         # we don't want to waste time generating children for invalid nodes
         if self.is_valid():
-
-            return []
+            next_hand_loc = self._hand_loc + 1 % self._hands
+            for move in self._allowed_moves:
+                new_move_red = self._new_move(self._state[self.RED_INDEX], move)
+                new_move_blue = self._new_move(self._state[self.BLUE_INDEX], move)
+                new_node = self._new_child_node(new_move_red, new_move_blue, next_hand_loc)
+                child_nodes.append(new_node)
 
         return child_nodes
 
